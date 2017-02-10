9402bc66eef4fa58ce55ca1b182dbb4681238d17
Hikmat Dhamee <hikmatdhamee@gmail.com>
2017-02-10 12:41:29 +0545

    adding demo for cc

--- WatUrlExtractor/src/main/java/cc/scrub/mapper/WatUrlExtractorMapper.java
+++ WatUrlExtractor/src/main/java/cc/scrub/mapper/WatUrlExtractorMapper.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.log4j.Logger;
 import org.archive.io.ArchiveReader;
 import org.archive.io.ArchiveRecord;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,7 +21,10 @@ import java.net.URI;
  */
 public class WatUrlExtractorMapper extends Mapper<Text, ArchiveReader, Text, NullWritable> {
     private static final Logger LOG = Logger.getLogger(WatUrlExtractorMapper.class);
+
     private static final String RGX_URL = "^(http[s]?:\\/\\/)?(www[.])?[a-z0-9]+([\\-][a-z0-9]+)?([.][a-z0-9]+)*(\\.com|\\.net|\\.org)([.][a-z]{1,3})?((?=\\/).*)?$";
+    private static final String SEARCH_WORD = "contact";
+    private static final String SEARCH_ATTRIBUTE = "url";
 
     private Text outKey = new Text();
     private NullWritable outVal = NullWritable.get();
@@ -37,13 +41,16 @@ public class WatUrlExtractorMapper extends Mapper<Text, ArchiveReader, Text, Nul
                 // Convenience function that reads the full message into a raw byte array
                 byte[] rawData = IOUtils.toByteArray(record, record.available());
                 String content = new String(rawData);
+
                 JSONObject json = new JSONObject(content);
+
                 try {
                     String siteUrl = json.getJSONObject("Envelope").getJSONObject("WARC-Header-Metadata").getString("WARC-Target-URI");
                     //JSON xpath = [Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']
-                    String inPageLinks = json.getJSONObject("Envelope").getJSONObject("Payload-Metadata").getJSONObject("HTTP-Response-Metadata").getJSONObject("HTML-Metadata").getJSONArray("Links").toString();
+                    JSONArray linksJsonArray = json.getJSONObject("Envelope").getJSONObject("Payload-Metadata").getJSONObject("HTTP-Response-Metadata")
+                            .getJSONObject("HTML-Metadata").getJSONArray("Links");
 
-                    if (inPageLinks.contains("contact") && siteUrl.matches(RGX_URL)) {
+                    if (siteUrl.matches(RGX_URL) && searchWord(linksJsonArray,SEARCH_ATTRIBUTE,SEARCH_WORD)) {
                         outKey.set(new URI(siteUrl).getHost()); // extract root url
                         context.write(outKey, outVal);
                     }
@@ -56,4 +63,14 @@ public class WatUrlExtractorMapper extends Mapper<Text, ArchiveReader, Text, Nul
         }
     }
 
+    private boolean searchWord(JSONArray searchSpaceArray,String attribute,String searchWord)throws JSONException {
+        boolean found = false;
+        for (int i = 0; i < searchSpaceArray.length(); i++) {
+            if(((JSONObject)(searchSpaceArray.get(i))).getString(attribute).contains(searchWord)){
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
 }
